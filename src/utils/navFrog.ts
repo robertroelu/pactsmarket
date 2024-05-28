0d9126b18c5f14da3a3f8bd7b0a04b2a83c8eac6
Róbert Klimant <roboklimant@gmail.com>
2024-05-28 15:29:32 +0200

    feat: adding automation rotation

--- src/utils/navFrog.ts
+++ src/utils/navFrog.ts
@@ -23,4 +23,37 @@ export const navFrog = () => {
       imageFrog.style.zIndex = '10';
     });
   });
+
+  // FOR ROTATION TABS
+  // Start Tabs
+  let tabTimeout: number | undefined;
+  clearTimeout(tabTimeout);
+  tabLoop();
+
+  // Connect your class names to elements.
+  function tabLoop() {
+    tabTimeout = window.setTimeout(() => {
+      const currentTab = document.querySelector('.tabs-menu .w--current') as HTMLElement;
+      const nextTab = currentTab?.nextElementSibling as HTMLElement;
+
+      if (nextTab) {
+        nextTab.click(); // Simulate a click on the next tab
+      } else {
+        const firstTab = document.querySelector('.tab-button:first-child') as HTMLElement;
+        if (firstTab) {
+          firstTab.click(); // Simulate a click on the first tab if no next tab is found
+        }
+      }
+    }, 10000); // 10 Second Rotation
+  }
+
+  // Reset Loops
+  document.querySelectorAll('.tab-button').forEach((tab) => {
+    tab.addEventListener('click', () => {
+      if (tabTimeout !== undefined) {
+        clearTimeout(tabTimeout);
+      }
+      tabLoop();
+    });
+  });
 };
